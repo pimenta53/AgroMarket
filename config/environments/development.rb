6593a6389369eb4ea73c01bb75b149518d346294
frakinho <paulo.4624@gmail.com>
2013-10-25 20:08:22 +0100

    [Add Feature] Email Config [Development]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Agrosocial::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,4 +26,16 @@ Agrosocial::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # email configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "agro.social2",
+    password: "agrosocial2"
+  }
 end
