c5906a302fcaea4a4bfabd2554f57f2d31a47ec0
frakinho <paulo.4624@gmail.com>
2013-10-25 20:38:50 +0100

    [Update Feature] Name validate [Development]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
