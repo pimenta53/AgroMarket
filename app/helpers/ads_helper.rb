2a9369673edddad1af861a5c9bd5a80c0b5e18f9
raphael.rodrigues <raphaeljr28@gmail.com>
2013-11-03 16:04:50 +0000

    [Design] Ads in thumbnails

--- app/helpers/ads_helper.rb
+++ app/helpers/ads_helper.rb
@@ -1,2 +1,19 @@
 module AdsHelper
+
+	def first_image( image )
+		 if !image.blank?
+             image_tag image.image(:small) ,:style => "width: 66%; height: 40%;", :alt => "200x100"
+         else
+         	 image_tag "http://placehold.it/200x100" ,:style => "width: 66%; height: 40%;", :alt => "200x100"
+         end
+	end
+
+
+	def ad_image( image )
+		 if !image.blank?
+             image_tag image.image(:small) , :style => "width: 100%; height: 100%;", :alt => "300x200"
+         else
+         	 image_tag "http://placehold.it/200x150" ,:style => "width: 100%; height: 100%;", :alt => "200x150"
+         end
+	end
 end
