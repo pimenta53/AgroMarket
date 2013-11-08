794fb717624ae76a61707ac322c4afdb8284fb4e
raphael.rodrigues <raphaeljr28@gmail.com>
2013-11-08 15:32:25 +0000

    [Added Feature]JavaScript Time Ago in words

--- spec/models/ad_spec.rb
+++ spec/models/ad_spec.rb
@@ -25,14 +25,43 @@ describe Ad do
 	      :price => 19.99,
 	      :expire_date => "2013/12/12",
 	      :type_price_id => 1,
-	      :city_id => 1
+	      :city_id => 1,
+	      :category_id => 1
 	    )
 	    @user = User.new(:id => 3)
 	end
 
+	#novo anuncio com 
+	describe 'A new ad' do
+		require 'watir-webdriver'
+		b = Watir::Browser.new
+		b.goto 'localhost:3000/ads/new'
+		
+		s = b.select_list :name => 'ad[category_id]'
+		s.select 'Agricultura'
+
+		b.text_field(:name => 'ad[title]').set 'Legumes Novos'
+		b.text_field(:name => 'ad[description]').set 'conjunto de legumes'
+		b.text_field(:name => 'ad[price]').set '100'
+		b.text_field(:name => 'ad[location]').set 'Legumes Novos'
+		sleep 1
+		s = b.select_list :name => 'ad[type_price_id]'
+		s.select 'Kg'
+		
+		sleep 1
+		s = b.select_list :name => 'ad[city_id]'
+		s.select 'Braga'
+
+		btn = b.button :value => 'Submit'
+		btn.click
+		
+		sleep 5
+		
+		
+	end
 
 	describe 'A new ad' do
-		  it "expiration date can´t be in the past" do
+		  it "expiration date cant be in the past" do
 		    @ad.expire_date = "2013/10/12"
 		    @ad.should_not be_valid
 		  end
