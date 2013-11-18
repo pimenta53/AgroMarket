cb1fb0f1e404a2436643d2fc8b73bc2fb33c294c
Mickael <dinomickael@gmail.com>
2013-11-18 23:08:47 +0000

    EditUser[View]

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,28 @@
 class RegistrationsController < Devise::RegistrationsController
-    layout "sessions"
+    layout :resolve_layout
   	before_action :load_cities, only: [:new, :create, :edit, :update]
   	before_action :date_convert, only: [:create,:update]
 
+  	def new
+  		super
+  	end
+
+
+
 	private 
+
+		def resolve_layout
+		    case action_name
+		    when "new", "create"
+		      "sessions"
+		    when "index"
+		      "sessions"
+		    else
+		      "application"
+		    end
+		end
+
+
 		def load_cities
 			@cities = City.all
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20131113173259) do
 
   create_table "ad_images", force: true do |t|
