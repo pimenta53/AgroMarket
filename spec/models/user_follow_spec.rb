d64c722db685360314e2b7abfb632ac5db3674f8
André Liu <andre_liu507@hotmail.com>
2013-11-20 16:42:12 +0000

    testing

--- spec/models/user_follow_spec.rb
+++ spec/models/user_follow_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe UserFollow do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before(:each) do
+	    @user1 = User.new(:id => 1)
+	    @user2 = User.new(:id => 2)
+	    @follow = UserFollow.new( 
+	      :user_id => 1, 
+	      :following_id => 2
+	    )
+	end
+	
+	describe 'A new follow' do
+		it "creation should be valid" do
+		   @follow.should be_valid
+	   end
+	end
+	
+	describe 'A new follow' do
+		it "shouldn't follow himself" do
+	      @follow.user_id = 1 
+	      @follow.following_id = 1
+			@follow.should_not be_valid
+	   end
+	end
 end
