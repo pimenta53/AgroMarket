024c3a646e29b859eb35c54951ebfee82d9f7bc9
André Liu <andre_liu507@hotmail.com>
2013-12-02 17:24:41 +0000

    google omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider, id, secret
   provider :facebook, '547514635336381', '037623bb1bd76b7b9e550213cd3c22b4'
   provider :twitter, 'Hl0gtbxf9mMB5sGd37VtA', '8793bRFsdZsZGPOcZnWCdKDXDdU76WQX09d8tUc5Po'
+  provider :google_oauth2, '611864901662.apps.googleusercontent.com', 'OtA8_4UzYIZWBRcAwhOU67k2'
   #provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
