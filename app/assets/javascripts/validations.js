c04156d14d18788b50e010eb6c0b455d1907c6fc
Carlos Martins <martinscmb@gmail.com>
2013-12-06 11:50:58 +0000

    [Bug Fixed] [36] new user validation: 'limitar textboxs'

--- app/assets/javascripts/validations.js
+++ app/assets/javascripts/validations.js
@@ -1,45 +1,19 @@
 var ready;
 ready = function() {
 
-  /**
-  * New USER, CREATE ACCOUNT
-  */
-
+  /******       New USER, CREATE ACCOUNT      */
   $('#new_user').validate({
     errorElement: 'label',
     errorClass: 'help-inline',
     rules: {
-      "user[email]": {
-        email: true,
-        required: true
-      },
-      "user[avatar]": {
-          required: false
-      },
-      "user[name]": {
-        accept: "[a-zA-Z ]",
-        required: true
-      },
-      "user[city_id]": {
-        required: true
-      },
-      "user[password]": {
-        minlength: 5,
-        maxlength: 15,
-        required: true
-      },
-      "user[password_confirmation]": {
-          minlength: 5,
-          maxlength: 15,
-          required: true
-      },
-      "user[birthday]": {
-          date: true,
-          required: true
-      },
-      "user[phone]": {
-          telefone: true
-      }
+      "user[email]": { email: true, maxlength: 255, required: true },
+      "user[avatar]": { required: false, maxlength: 255 },
+      "user[name]": { accept: "[a-zA-Z ]", required: true, maxlength: 255 },
+      "user[city_id]": { required: true },
+      "user[password]": {minlength: 5, maxlength: 15, required: true },
+      "user[password_confirmation]": { minlength: 5, maxlength: 15, required: true },
+      "user[birthday]": {date: true, required: true },
+      "user[phone]": { telefone: true }
     },
     highlight: function(element) {
       $(element).closest('.control-group').removeClass('success').addClass('error');
@@ -51,18 +25,12 @@ ready = function() {
       }
    });
 
-    /**
-     * REQUEST PASSWORD
-     */
-
+    /*****      REQUEST PASSWORD            */
     $('#request_password').validate({
         errorElement: 'label',
         errorClass: 'help-inline',
         rules: {
-            "user[email]": {
-                email: true,
-                required: true
-            }
+            "user[email]": { email: true, required: true }
         },
         highlight: function(element) {
             $(element).closest('.control-group').removeClass('success').addClass('error');
@@ -75,25 +43,14 @@ ready = function() {
 
     });
 
-  /**
-  * NEW CATEGORY
-  */
-
+  /*****        NEW CATEGORY                */
   $('#new_category').validate({
     errorElement: 'label',
     errorClass: 'help-inline',
     rules: {
-      "category[name]": {
-        required: true,
-        minlength: 3
-      },
-      "category[color]": {
-        required: true
-      },
-      "category[description]": {
-        required: true,
-        minlength: 5
-      }
+      "category[name]": { required: true, minlength: 3 },
+      "category[color]": { required: true },
+      "category[description]": { required: true, minlength: 5 }
     },
     messages: {
       "category[color]": "Adicione uma cor."
@@ -109,17 +66,12 @@ ready = function() {
 
   });
 
-  /**
-  * NEW CITY
-  */
-
+  /*****        NEW CITY                */
   $('#new_city').validate({
     errorElement: 'label',
     errorClass: 'help-inline',
     rules: {
-      "city[city]": {
-        minlength: 4
-      }
+      "city[city]": { minlength: 4 }
     },
     highlight: function(element) {
       $(element).closest('.control-group').removeClass('success').addClass('error');
@@ -132,48 +84,19 @@ ready = function() {
 
   });
 
-  /*****************************************************************
-  * NEW AD
-  *****************************************************************/
+  /*****        NEW AD                             */
   $('#ad-form').validate({
     errorElement: 'label',
     errorClass: 'help-inline',
     rules: {
-
-      "ad[category_id]": {
-        required: true
-      },
-      "ad[title]": {
-        minlength: 2,
-        maxlength: 20,
-        required: true
-      },
-      "ad[description]": {
-        minlength: 5,
-        maxlength: 140,
-        required: true
-      },
-      "ad[price]": {
-        minlength: 1,
-        maxlength: 6,
-        number: true,
-        required: true
-      },
-      "ad[expire_date]": {
-        date: true,
-        required: true
-      },
-      "ad[location]": {
-        minlength:2,
-        maxlength: 40,
-        required: true
-      },
-      "ad[type_price_id]": {
-        required: true
-      },
-      "ad[city_id]": {
-        required: true
-      }
+      "ad[category_id]": { required: true },
+      "ad[title]": {minlength: 2, maxlength: 50, required: true },
+      "ad[description]": { minlength: 5, maxlength: 255, required: true },
+      "ad[price]": { minlength: 1, maxlength: 6, number: true, required: true },
+      "ad[expire_date]": { date: true, required: true },
+      "ad[location]": { minlength:2, maxlength: 255, required: true },
+      "ad[type_price_id]": { required: true },
+      "ad[city_id]": { required: true }
     },
       highlight: function(element) {
           $(element).closest('.control-group').removeClass('success').addClass('error');
@@ -186,8 +109,8 @@ ready = function() {
   });
 
 
-  /**
-  * New AD ADmin zone
+  /*****
+  * New AD ADmin zone    (pq é que nao se usa a mesma validação do new ad ?)
   */
 
   $('#new_ad').validate({
