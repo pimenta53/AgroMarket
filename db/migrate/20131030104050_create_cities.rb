bb96de25f684d39ba954577546748d2a06eaf829
HFPS <helderfpsilva@gmail.com>
2013-12-16 13:08:47 +0000

    District/city schema

--- db/migrate/20131030104050_create_cities.rb
+++ db/migrate/20131030104050_create_cities.rb
@@ -1,8 +1,8 @@
 class CreateCities < ActiveRecord::Migration
   def change
     create_table :cities do |t|
-      t.string :city
-      t.integer :country_id
+      t.string  :city
+      t.integer :district_id
 
       t.timestamps
     end
