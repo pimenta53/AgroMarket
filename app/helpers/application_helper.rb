255c221d269dec8759a85d438d349dc78a66f169
André Liu <andre_liu507@hotmail.com>
2014-01-08 14:08:03 +0000

    Publicidade

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
 	    controller.class.name.split("::").first=="Academy"
 	end
 
+   #Publicity globals
+   $publicity_offset = 0   
+   
+   def get_publicity(count)
+     fetch_result = Admin::Publicity.get_publicity_with_offset($publicity_offset, count)
+      
+     $publicity_offset = fetch_result["offset"]
+     fetch_result["list"]
+   end
 end
--- app/models/admin/publicity.rb
+++ app/models/admin/publicity.rb
@@ -1,3 +1,9 @@
+#      t.string :title
+#      t.string :description
+#      t.datetime :expire_date
+#      t.string :owner
+#      t.boolean :is_deleted
+
 class Admin::Publicity < ActiveRecord::Base
 	has_attached_file :image,:styles => {
 		:avatar => "50x50!",
@@ -7,4 +13,45 @@ class Admin::Publicity < ActiveRecord::Base
 		:large => "600x600!"
 	}
 
+   def self.get_publicity(index, count)
+     self.get_publicity_with_offset(index, count)["list"]
+   end
+
+   def self.get_publicity_with_offset(index, count)
+   
+     list = Admin::Publicity.where("is_deleted = false").limit(count).offset(index)
+     
+     new_offset = index + list.length
+     
+     if !(count == 0 || (list.length == 0 && index == 0))
+       # se nao buscou publicidade suficiente
+       if (list.length < count)
+       
+         #Verificar se iniciou no inicio da lista
+         if (index == 0)
+           #Este caso só acontece se o numero de publicidade existente é menor que a pedida, usamos lista com repetições
+           newlist = list 
+           while (newlist.length < count) do
+             #adicionar o numero de elementos que falta
+             new_offset = count - newlist.length
+             if list.length < new_offset
+               #não será necessário usa o offset porque é maior que a lista
+               newlist = newlist + list
+             else
+               newlist = newlist + list.take(new_offset)
+             end
+           end
+           
+           list = newlist
+         else
+           #Este caso acontecerá se atingiu o fundo da lista de publicidades, voltamos ao inicio
+           newlist = Admin::Publicity.get_publicity_with_offset(0, count - list.length)
+           
+           list = list + newlist["list"]
+           new_offset = newlist["offset"]
+         end
+       end
+     end
+     Hash["list", list, "offset", new_offset]
+   end
 end
