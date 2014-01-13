86ab0d04f5e4711bcfd7299f7352c23ce8cf8115
Mário Pinto <mariobpinto@gmail.com>
2014-01-13 18:27:02 +0000

    [Update && fix] route cleaning

--- app/views/price_types/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@price_types) do |price_type|
-  json.extract! price_type, :name
-  json.url price_type_url(price_type, format: :json)
-end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: payments
+#
+#  id         :integer          not null, primary key
+#  ref        :string(255)      not null
+#  user_id    :integer          not null
+#  used       :boolean          default(FALSE)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Payment do
