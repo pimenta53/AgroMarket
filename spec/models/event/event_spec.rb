1a348cafc7318a2c1f766da3939647b0d6880b6a
Mário Pinto <mariobpinto@gmail.com>
2014-01-15 13:35:54 +0000

    [Update Feature] Event soft delete

--- spec/models/event/event_spec.rb
+++ spec/models/event/event_spec.rb
@@ -2,16 +2,21 @@
 #
 # Table name: event_events
 #
-#  id          :integer          not null, primary key
-#  start_day   :datetime
-#  end_day     :datetime
-#  title       :string(255)      not null
-#  description :text
-#  user_id     :integer
-#  aproved     :boolean          default(FALSE)
-#  created_at  :datetime
-#  updated_at  :datetime
-#  city_id     :integer
+#  id                 :integer          not null, primary key
+#  start_day          :datetime
+#  end_day            :datetime
+#  title              :string(255)      not null
+#  description        :text
+#  user_id            :integer
+#  aproved            :boolean          default(FALSE)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  city_id            :integer
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  deleted            :boolean
 #
 
 require 'spec_helper'
