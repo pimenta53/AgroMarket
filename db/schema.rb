c708949f9586846459f90b9a33ae2dc493570d87
Mário Pinto <mariobpinto@gmail.com>
2014-01-20 16:19:45 +0000

    [Bug fix] edit events, tutorials, workshops in Admin_Zone

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140120121646) do
 
-
   create_table "academy_answers", force: true do |t|
     t.integer  "question_id",                 null: false
     t.integer  "user_id",                     null: false
@@ -281,13 +280,6 @@ ActiveRecord::Schema.define(version: 20140120121646) do
     t.integer  "user_two",               null: false
     t.integer  "ad_id"
     t.integer  "is_close",   default: 0
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "types", force: true do |t|
-    t.string   "name"
-    t.integer  "flag"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
