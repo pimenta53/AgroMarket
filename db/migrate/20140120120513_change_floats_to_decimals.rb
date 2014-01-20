798d7b619af62120c1b0711ada039ad882a26099
HFPS <helderfpsilva@gmail.com>
2014-01-20 12:11:14 +0000

    [Bug Fix]

--- db/migrate/20140120120513_change_floats_to_decimals.rb
+++ db/migrate/20140120120513_change_floats_to_decimals.rb
@@ -1,5 +1,6 @@
 class ChangeFloatsToDecimals < ActiveRecord::Migration
   def change
     change_column :academy_workshops, :price, :decimal, :precision => 10, :scale => 2
+    change_column :ads,               :price, :decimal, :precision => 10, :scale => 2
   end
 end
