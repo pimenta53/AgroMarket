087c77856e8e7548a52feace94ed074dfe790396
Mickael <dinomickael@gmail.com>
2014-01-20 14:38:29 +0000

    UpdatePerfil

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -31,6 +31,7 @@ class RegistrationsController < Devise::RegistrationsController
       def build_resource(*args)
         super
         if session[:omniauth]
+
           @user.apply_omniauth(session[:omniauth])
           @user.valid?
         end
