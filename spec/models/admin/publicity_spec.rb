77486848413d2594dd66a7ca3b42273937f0757e
Mário Pinto <mariobpinto@gmail.com>
2014-01-22 15:36:02 +0000

    [Bug Fix] User soft-delete

--- spec/models/admin/publicity_spec.rb
+++ spec/models/admin/publicity_spec.rb
@@ -1,12 +1,13 @@
 # == Schema Information
 #
-# Table name: admin_publicities
+# Table name: publicities
 #
 #  id                 :integer          not null, primary key
 #  title              :string(255)
 #  description        :string(255)
 #  expire_date        :datetime
 #  owner              :string(255)
+#  link               :string(255)
 #  is_deleted         :boolean
 #  created_at         :datetime
 #  updated_at         :datetime
@@ -14,7 +15,6 @@
 #  image_content_type :string(255)
 #  image_file_size    :integer
 #  image_updated_at   :datetime
-#  link               :string(255)
 #
 
 require 'spec_helper'
